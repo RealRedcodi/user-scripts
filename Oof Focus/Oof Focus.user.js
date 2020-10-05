5c2067999aee96ab1199c6b24bf26317e06b03a3
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-05 10:10:22 -0400

    Update Oof Focus.user.js

--- Oof Focus/Oof Focus.user.js
+++ Oof Focus/Oof Focus.user.js
@@ -12,8 +12,8 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require      https://greasyfork.org/scripts/407905-loggerjs/code/LoggerJS.js?version=831993
 // @require      https://greasyfork.org/scripts/411028-oof-api/code/OOF%20API.user.js
-// @updateURL    https://github.com/RealRedcodi/user-scripts/raw/main/Oof%20Focus/Oof%20Focus.user.js
-// @downloadURL  https://github.com/RealRedcodi/user-scripts/raw/main/Oof%20Focus/Oof%20Focus.user.js
+// @updateURL    https://raw.githubusercontent.com/RealRedcodi/user-scripts/main/Oof%20Focus/Oof%20Focus.user.js
+// @downloadURL  https://raw.githubusercontent.com/RealRedcodi/user-scripts/main/Oof%20Focus/Oof%20Focus.user.js
 // @grant        none
 // ==/UserScript==
 
