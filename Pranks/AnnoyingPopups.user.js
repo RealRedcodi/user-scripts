cd6139907e2837dd185b0862b6a1c2077cd10df9
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-05 10:49:25 -0400

    Create AnnoyingPopups.user.js

---
+++ Pranks/AnnoyingPopups.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Annoying Popups
+// @namespace    https://redcodi.space
+// @version      0.1
+// @description  Annoying Popups Prank!
+// @author       Redcodi
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
