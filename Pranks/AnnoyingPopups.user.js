5d7ff496fd7d1d76a80764bdcb4bf6d205ceb79c
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-05 11:00:55 -0400

    Update AnnoyingPopups.user.js

--- Pranks/AnnoyingPopups.user.js
+++ Pranks/AnnoyingPopups.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Annoying Popups
 // @namespace    https://redcodi.space
-// @version      0.4
+// @version      0.5
 // @description  Annoying Popups Prank!
 // @author       Redcodi
 // @supportURL   https://github.com/RealRedcodi/user-scripts/issues
 // @homepage     https://github.com/RealRedcodi/user-scripts/blob/main/Pranks
 // @require      https://unpkg.com/sweetalert/dist/sweetalert.min.js
 // @match        http://*/*
-// @updateURL    https://raw.githubusercontent.com/RealRedcodi/user-scripts/main/main/Pranks/AnnoyingPopups.user.js
-// @downloadURL  https://raw.githubusercontent.com/RealRedcodi/user-scripts/main/main/Pranks/AnnoyingPopups.user.js
+// @updateURL    https://raw.githubusercontent.com/RealRedcodi/user-scripts/main/Pranks/AnnoyingPopups.user.js
+// @downloadURL  https://raw.githubusercontent.com/RealRedcodi/user-scripts/main/Pranks/AnnoyingPopups.user.js
 // @grant        none
 // ==/UserScript==
 
